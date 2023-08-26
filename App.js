e3091dd734850bca7c5e28e271cb5db32f8603f9
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-08-26 12:25:12 +0530

    Props Complete

--- App.js
+++ App.js
@@ -2,19 +2,29 @@ import React, { useState } from 'react';
 import { Text,View,Button} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState("Md Somad");
-
-  function testName(){
-    setName("Updated Name: Sanchita")
-  }
-
-
+  const [name, setName] = useState("Somad")
   return (
     <View>
-      <Text style={{fontSize:30}}>{name}</Text>
-      <Button title='Update Name' color={"red"} onPress={testName}></Button>
+      <Text style={{fontSize:30}}>Props in React native</Text>
+      <Button title='Update name' onPress={()=> setName("Yasin")}></Button>
+      <User name={name} age={20}/>
     </View>
   );
 };
+
+
+const User = (Props)=>{
+  console.warn(Props.name)
+  return(
+    <View style={{backgroundColor:"green",padding:5}}>
+
+      <Text style={{fontSize:30}}>Name: {Props.name}</Text>
+      <Text style={{fontSize:30}}>Age: {Props.age}</Text>
+      
+    </View>
+  )
+}
+
+
 
 export default App;
